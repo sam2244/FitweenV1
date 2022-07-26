6159606653b1b2328137d93c17db47eded23e3c1
sam2244 <phenix2244@naver.com>
2022-07-26 16:45:40 +0900

    main page app bar, floating button setup

--- lib/view/main/main.dart
+++ lib/view/main/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:fitweenV1/view/main/widget.dart';
+
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+    return const Scaffold(
+      appBar: MainAppBar(),
+      body: Center(child: Text('아직 크루가 없어요!')),
+      floatingActionButton: MainFloatingButton(),
     );
   }
 }
